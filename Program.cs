13484b5ed358fa9622789f6fdc543eb0a3185f83
Cody_Jones <cbjones93@gmail.com>
2021-06-08 15:36:27 -0500

    created bank object

--- Program.cs
+++ Program.cs
@@ -7,64 +7,127 @@ namespace HeistPartII
     {
         static void Main(string[] args)
         {
+          
+                Random i = new Random();
+                Bank bank = new Bank()
+                {
+                    CashOnHand = i.Next(50000, 1000000),
+                    AlarmScore = i.Next(0,100),
+                    VaultScore = i.Next(0,100),
+                    SecurityGuardScore= i.Next(0,100)
+                 
+                };
+            
             List<IRobber> rolodex = new List<IRobber>();
-            void Create()
-            {
             Muscle muscle1 = new Muscle()
             {
                 Name = "Chad",
-                SkillLevel= 50,
-                PercentageCut= 25
+                SkillLevel = 50,
+                PercentageCut = 25
 
             };
-           rolodex.Add(muscle1);
-         
+            rolodex.Add(muscle1);
+
             Muscle muscle2 = new Muscle()
-                {
+            {
                 Name = "Larry",
-                SkillLevel= 60,
-                PercentageCut= 25
+                SkillLevel = 60,
+                PercentageCut = 25
             };
-             rolodex.Add(muscle2);
+            rolodex.Add(muscle2);
             Hacker hacker1 = new Hacker()
-             {
+            {
                 Name = "Zip",
-                SkillLevel= 40,
-                PercentageCut= 25
+                SkillLevel = 40,
+                PercentageCut = 25
 
             };
-             rolodex.Add(hacker1);
+            rolodex.Add(hacker1);
             Hacker hacker2 = new Hacker()
-             {
+            {
                 Name = "Zeek",
-                SkillLevel= 40,
-                PercentageCut= 25
+                SkillLevel = 40,
+                PercentageCut = 25
 
             };
             rolodex.Add(hacker2);
             LockSpecialist lock1 = new LockSpecialist()
-             {
+            {
                 Name = "Charles",
-                SkillLevel= 70,
-                PercentageCut= 50
+                SkillLevel = 70,
+                PercentageCut = 50
 
             };
             rolodex.Add(lock1);
             LockSpecialist lock2 = new LockSpecialist()
-             {
+            {
                 Name = "Dave",
-                SkillLevel= 45,
-                PercentageCut= 25
+                SkillLevel = 45,
+                PercentageCut = 25
 
             };
             rolodex.Add(lock2);
+            void Create()
+            {
+
+
+
+                Console.WriteLine($"There are {rolodex.Count} members in your rolodex");
+                Console.WriteLine("Add a new member");
+                Console.WriteLine("Name:");
+                string name = Console.ReadLine();
+                if (name.Length > 0)
+                {
+                    Console.WriteLine(@"Choose a class >
+                1) Hacker (Disables alarms)
+                2) Muscle (Disarms guards)
+                3)Lock Specialist (Cracks vault)");
+                    int speciality = Int32.Parse(Console.ReadLine());
+                    Console.WriteLine(@"Enter skill level (between 1 and 100)");
+                    int skill = Int32.Parse(Console.ReadLine());
+                    Console.WriteLine($"Enter {name}'s cut of the cash. (between 1 and 100%)");
+                    int cut = Int32.Parse(Console.ReadLine());
+                    if (speciality == 1)
+                    {
+                        Hacker hacker3 = new Hacker()
+                        {
+                            Name = name,
+                            SkillLevel = skill,
+                            PercentageCut = cut
+                        };
+                        rolodex.Add(hacker3);
+                    }
+                    else if (speciality == 2)
+                    {
+                        Muscle muscle3 = new Muscle()
+                        {
+                            Name = name,
+                            SkillLevel = skill,
+                            PercentageCut = cut
+                        };
+                        rolodex.Add(muscle3);
+                    }
+                    else if (speciality == 3)
+                    {
+                        LockSpecialist lock3 = new LockSpecialist()
+                        {
+                            Name = name,
+                            SkillLevel = skill,
+                            PercentageCut = cut
+                        };
+                        rolodex.Add(lock3);
+                    }
+                    Create();
+                }
+                else
+                {
+
+                }
             }
             Create();
-            Console.WriteLine($"There are {rolodex.Count} members in your rolodex");
-
-          
-
+            
         }
-    }
 
+
+    }
 }
